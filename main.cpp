d5eae72b010b6fbb5335cb6d0c955f87b4d497b8
Aniket Chowdhudy <aniket_chowdhury@hotmail.com>
2018-06-18 09:22:41 +0530

    Update main.cpp
    Adding custom input from stdin

--- main.cpp
+++ main.cpp
@@ -3,25 +3,51 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <stdexcept>
 
 using namespace std;
 
-int main()
+template <class T>
+vector<float> minmaxstandard(vector<T> v)
 {
-    vector<float> v(10,0);
-    vector<float> vStandard(10,0);
-    srand(static_cast<uint16_t>(time(0)));
-    for(auto& elem:v)
-        elem = rand()%100;
+    if(v.size()==1) throw std::runtime_error("Not a range");
 
     sort(v.begin(),v.end());
-    int min = v[0];
-    int max = v[9];
 
-    for(int i=0; i<10; i++)
+    float min = v.front();
+    float max = v.back();
+
+    vector<float> v1(v.begin(),v.end());
+
+    for(size_t i=0; i<v1.size(); i++)
     {
-        vStandard[i] = (v[i]-min)/(max-min)*(1-0)+0;
-        cout << v[i] << "\t|\t" << vStandard[i] << "\n";
+        v1[i] = (v1[i]-min)/(max-min)*(1-0)+0;
     }
+    return v1;
+}
 
+int main()
+{
+    vector<int> v = {8,10,15,20};
+
+    ///Uncomment this part for custom input
+
+        std::cout << "Enter T(size of array):";
+        size_t n;
+        std::cin >> n;
+
+        vector<int> v1(n,0);
+
+        for(size_t i=0; i<v1.size(); i++)
+            std::cin >> v1[i];
+
+    v = v1;
+
+    auto vStandard = minmaxstandard(v);
+
+    for(size_t i=0; i<v.size(); i++)
+    {
+        cout << v[i] << "\t|\t" << vStandard[i] <<"\n";
+
+    }
 }
